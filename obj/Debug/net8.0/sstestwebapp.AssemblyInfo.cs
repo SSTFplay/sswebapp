f86db20c0ff2f80a3abb1c6125d256209798a0be
sheraysinghavanade <42808402+sheraysinghavanade@users.noreply.github.com>
2024-09-28 16:52:54 -0400

    2nd Commit

--- obj/Debug/net8.0/sstestwebapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/sstestwebapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sstestwebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89575459303031f3b8c916f1241b881716533914")]
 [assembly: System.Reflection.AssemblyProductAttribute("sstestwebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sstestwebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
