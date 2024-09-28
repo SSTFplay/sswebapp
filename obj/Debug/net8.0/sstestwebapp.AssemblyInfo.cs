d7f34b67b04ca3e173fa63ac974bcc4b697993ae
sheraysinghavanade <42808402+sheraysinghavanade@users.noreply.github.com>
2024-09-28 17:16:52 -0400

    Updated gitIgnore

--- obj/Debug/net8.0/sstestwebapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/sstestwebapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sstestwebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1aca0468166f559cc144cbff411c686e51c3190")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae66181f160cd64f854cb03bd48de14d7fcc8b14")]
 [assembly: System.Reflection.AssemblyProductAttribute("sstestwebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sstestwebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
